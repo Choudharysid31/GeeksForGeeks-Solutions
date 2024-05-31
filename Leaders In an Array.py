70f1ed717573484a54fc42ffd52d1493705a4d70
Siddharth Kumar Choudhary <135629931+Choudharysid31@users.noreply.github.com>
2024-06-01 03:37:45 +0530

    Update Leaders In an Array.py

--- Leaders In an Array.py
+++ Leaders In an Array.py
@@ -1,3 +1,19 @@
+'''Given an array A of positive integers. Your task is to find the leaders in the array. An element of the array is a leader if it is greater than all the elements to 
+its right side or if it is equal to the maximum element on its right side. The rightmost element is always a leader. 
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.'''
+
 class Solution:
     def leaders(self, A, N):
         #Code here
