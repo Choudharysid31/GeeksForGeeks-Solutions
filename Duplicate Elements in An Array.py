bced9a71ca967295e9e92c890e60f6cae3a50311
Siddharth Kumar Choudhary <135629931+Choudharysid31@users.noreply.github.com>
2024-06-01 03:36:57 +0530

    Update Duplicate Elements in An Array.py

--- Duplicate Elements in An Array.py
+++ Duplicate Elements in An Array.py
@@ -1,3 +1,19 @@
+'''Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. 
+Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+
+Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array. 
+
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: 
+-1
+Explanation: 
+There is no repeating element in the array. Therefore output is -1.'''
+
+
 class Solution:
     def duplicates(self, arr, n):
         dups = []
