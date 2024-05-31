7566c98a129eb65b4c8e12bdfa993989bb23e435
Siddharth Kumar Choudhary <135629931+Choudharysid31@users.noreply.github.com>
2024-06-01 03:39:49 +0530

    Update Second Largest in An Array.py

--- Second Largest in An Array.py
+++ Second Largest in An Array.py
@@ -1,3 +1,14 @@
+'''Given an array Arr of size N, print the second largest distinct element from an array. If the second largest element doesn't exist then return -1.
+
+Example 1:
+Input: 
+N = 6
+Arr[] = {12, 35, 1, 10, 34, 1}
+Output: 34
+Explanation: The largest element of the 
+array is 35 and the second largest element
+is 34.'''
+
 class Solution:
 	def print2largest(self,arr, n):
 	    arr=sorted(set(arr))
