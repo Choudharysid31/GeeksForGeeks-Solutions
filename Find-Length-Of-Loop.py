fdd0fc47846d2dc71c2ae4172fe424e7d50f2200
Siddharth Kumar Choudhary <135629931+Choudharysid31@users.noreply.github.com>
2024-08-29 00:42:55 +0530

    Create Find-Length-Of-Loop.py

---
+++ Find-Length-Of-Loop.py
@@ -0,0 +1,38 @@
+'''Given the head of a linked list, determine whether the list contains a loop. If a loop is present, return the number of nodes in the loop, otherwise return 0.
+Note: 'c' is the position of the node which is the next pointer of the last node of the linkedlist. If c is 0, then there is no loop.
+
+Input: LinkedList: 25->14->19->33->10->21->39->90->58->45, c = 4
+Output: 7
+Explanation: The loop is from 33 to 45. So length of loop is 33->10->21->39-> 90->58->45 = 7. 
+The number 33 is connected to the last node of the linkedlist to form the loop because according to the input the 4th node from the beginning(1 based indexing) 
+will be connected to the last node for the loop.
+ 
+Input: LinkedList: 5->4, c = 0
+Output: 0
+Explanation: There is no loop.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)'''
+
+class Solution:
+    def countNodesInLoop(self, head):
+        slow=head
+        fast=head
+        flag=0
+        while fast is not None and fast.next is not None:
+            fast=fast.next.next
+            slow=slow.next
+            if slow==fast:
+                flag=1
+                break
+        if not flag:
+            return 0
+        count=1
+        slow=head
+        while slow !=fast:
+            slow=slow.next
+            fast=fast.next
+        while fast.next!=slow:
+            fast=fast.next
+            count+=1
+        return count
