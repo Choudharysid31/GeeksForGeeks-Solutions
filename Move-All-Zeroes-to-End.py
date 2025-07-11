89d0aca5e021d779d143194851e64cb350bbbf55
Siddharth Kumar Choudhary <135629931+Choudharysid31@users.noreply.github.com>
2025-07-12 00:06:04 +0530

    Create Move-All-Zeroes-to-End.py

---
+++ Move-All-Zeroes-to-End.py
@@ -0,0 +1,25 @@
+'''You are given an array arr[] of non-negative integers. Your task is to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+
+Input: arr[] = [10, 20, 30]
+Output: [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
+
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 105'''
+
+class Solution:
+	def pushZerosToEnd(self,arr):
+    	start=0
+    	for end in range(0,len(arr)):
+    	    if (arr[end]!=0):
+    	        arr[start],arr[end]=arr[end],arr[start]
+    	        start+=1
+    	return start
